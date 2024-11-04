ab2ec517bea02e165b84990624ad34714db896ff
David <davidisbest27@gmail.com>
2024-11-04 23:37:24 +0100

    add functional Image reference detection using ARWorldTracking

---
+++ app/ARapp/ARExperiment/ARControllers/HumanDetectionController.swift
@@ -0,0 +1,8 @@
+//
+//  HumanDetectionController.swift
+//  ARExperiment
+//
+//  Created by David Drobny on 30/10/2024.
+//
+
+import Foundation
---
+++ app/ARapp/ARExperiment/ARControllers/PasswordDetectionController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ARExperiment
+//
+//  Created by David Drobny on 27/10/2024.
+//
+
+import Foundation
