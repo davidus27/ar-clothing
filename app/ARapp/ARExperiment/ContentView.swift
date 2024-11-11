54f98793e0367ba66fcd0b3601822a2df3b3d278
David <davidisbest27@gmail.com>
2024-11-11 14:06:22 +0100

    add update to the project file

--- app/ARapp/ARExperiment/ContentView.swift
+++ app/ARapp/ARExperiment/ContentView.swift
@@ -9,24 +9,45 @@ import SwiftUI
 import ARKit
 import RealityKit
 
+class ImageDataModel: ObservableObject {
+    @Published var selectedImage: UIImage?
+}
+
 
 struct ContentView: View {
     @State private var showSheet: Bool = false
     @State private var activeTab: Tab = .designs
+    @State private var selectedImage: UIImage?
     
     var body: some View {
+        TakingImagesSelection(selectedImage: $selectedImage)
+        // SwiftUIView(selectedImage: $selectedImage)
+        
+        GenericUIViewControllerRepresentable(
+            bindingValue: $selectedImage,
+            makeUIViewController: {
+                TestingController()
+            },
+            updateUIViewController: { (controller, image) in
+                controller.selectedImage = image
+            }
+        )
+        
         ZStack(alignment: .bottom) {
             // AR Session
-            ARViewContainerConverter<DynamicReferenceController>().edgesIgnoringSafeArea(.all)
+//            TakingImagesScreen()
+            // ARViewContainerConverterTemporary<DynamicReferenceController>().edgesIgnoringSafeArea(.all)
+            
+            // ARViewContainerConverterTemporary<TestingController>()
             
             // Main menu
             TabBarView(activeTab: $activeTab)
         }
-        .task {
-            showSheet = true
+//        .task {
+//            showSheet = true
+//        }
+//        .sheet(isPresented: $showSheet) {
+//            SheetContentView(activeTab: $activeTab)
+//        }
         }
-        .sheet(isPresented: $showSheet) {
-            SheetContentView(activeTab: $activeTab)
-        }
-    }
 }
