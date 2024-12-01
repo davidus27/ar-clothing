51f5fd477d4aede08253494906cb791e5e7c2d25
David <davidisbest27@gmail.com>
2024-12-01 11:01:24 +0100

    add function to convert images to base64 for mockups

--- app/ARapp/ARExperiment/ContentView.swift
+++ app/ARapp/ARExperiment/ContentView.swift
@@ -11,31 +11,47 @@ import RealityKit
 
 struct ContentView: View {
     @State private var showSheet: Bool = false
-    @State private var activeTab: TabOption = .explore // default selected view at the start
+    @State private var activeTab: TabOption = .create // default selected view at the start
     @State private var sheetHeight = 0.0
     @State private var isGuideShown: Bool = false // this will normally be set to true
     @State private var appStatus: String = "Initializing..."
     
     var body: some View {
-        if isGuideShown {
-            AppGuideView(isGuideShown: $isGuideShown)
-        }
-        else {
-            ZStack(alignment: .bottom) {
-                MainARView(isGuideShown: $isGuideShown, appStatus: $appStatus)
-                
-                // Main menu
-                TabBarView(activeTab: $activeTab)
+        ZStack {
+            if isGuideShown {
+                AppGuideView(isGuideShown: $isGuideShown)
             }
-            .task {
-                appStatus = "Ready to explore"
-                showSheet = true
-            }
-            .sheet(isPresented: $showSheet) {
-                SheetContentView(activeTab: $activeTab)
+            else {
+                ZStack(alignment: .bottom) {
+                    MainARView(isGuideShown: $isGuideShown, appStatus: $appStatus)
+                    
+                    // Main menu
+                    TabBarView(activeTab: $activeTab)
+                }
+                .task {
+                    appStatus = "Ready to explore"
+                    showSheet = true
+                }
+                .sheet(isPresented: $showSheet) {
+                    SheetContentView(activeTab: $activeTab)
+                }
             }
         }
     }
+}
+
+// calculate the Base64 value of any image
+// this is just throw-away function in case we need mockup images
+func getBase64ImageValue(image: UIImage) -> String {
+    // Convert the image to PNG data
+    guard let pngData = image.pngData() else {
+       print("Failed to convert image to PNG data")
+       return ""
+    }
+
+    // Encode the PNG data to a Base64 string
+    let base64String = pngData.base64EncodedString()
+    return base64String
 }
 
 #Preview {
